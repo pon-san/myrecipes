107da180a5e6fbb4870aae6373a5b56d5902a6c5
Yuki Yamasaki <mttkd399@gmail.com>
2019-02-05 01:05:32 +0900

    add function of edit and delete recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,45 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new()
   end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:success] = 'Recipe was created successfully!'
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+  def edit 
+    @recipe = Recipe.find_by(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find_by(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = 'the recipe was successfully updated'
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find_by(params[:id])
+    if @recipe.destroy
+      flash[:success] = 'the recipe was successfully deleted'
+      redirect_to recipes_path
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @recipe = Recipe.create(name: 'test name', description: 'test description1')
+  end
+  
   test "should get recipes index" do
     get recipes_path
     assert_response :success
