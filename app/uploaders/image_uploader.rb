9c0a6e7e793400493ff34d66ca498c76df6be2ea
Yuki Yamasaki <mttkd399@gmail.com>
2019-02-07 00:34:32 +0900

    fix some UI

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  #
+  process resize_to_limit: [1200, 900]
   # def scale(width, height)
   #   # do something
   # end
